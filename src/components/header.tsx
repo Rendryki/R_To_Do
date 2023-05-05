18f21d5afb7fb9d92219e50e5146116339a9fc54
Rendryki Melo <rendrykifelipesilva@gmail.com>
2023-05-05 10:55:20 -0300

    commit inicial

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/header.module.css';
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <h1>React + TS + NextJS</h1>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/tasklist.tsx
@@ -0,0 +1,35 @@
+import { ITask } from "@/interfaces/task";
+import styles from '../styles/tasklist.module.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+interface Props {
+    taskList: ITask[],
+    handleDelete(id:number):void,
+    handleEdit(task:ITask): void
+}
+
+const TaskList = ({taskList, handleDelete, handleEdit}: Props) => {
+    return (
+        <>
+            {taskList.length > 0 ? (
+                taskList.map((task) => (
+                    <div key={task.id} className={styles.task}>
+                        <div className={styles.details}>
+                            <h4>{task.title}</h4>
+                            <p>Dificuldade: {task.difficulty}</p>
+                        </div>
+                        <div className={styles.actions}>
+                            <i className="bi bi-pencil" onClick={() => {handleEdit(task)}}></i>
+                            <i className="bi bi-trash" onClick={() => {handleDelete(task.id)}}></i>
+                            
+                        </div>
+                    </div>
+                )))
+            :
+            <p>nao tem tarefa cadastrada</p>
+            }
+        </>
+    )
+}
+
+export default TaskList;
